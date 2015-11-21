c8460bb1ca06f44dadaec54e925f012543f5fa78
ryutamaki <ryu.mochi@gmail.com>
2015-11-21 15:29:23 -0800

    Initial commit

---
+++ LaunchCountManager/LaunchCountManager.swift
@@ -0,0 +1,91 @@
+//
+//  LaunchCountManager.swift
+//  Vibalarm
+//
+//  Created by ryutamaki on 2015/10/06.
+//  Copyright © 2015年 ryutamaki. All rights reserved.
+//
+
+import UIKit
+
+class LaunchCountManager: NSObject {
+
+    static private let VERSIONS: String = "versions"
+
+    static let sharedManager: LaunchCountManager = LaunchCountManager()
+
+    static func launch() {
+        LaunchCountManager.sharedManager.launch()
+    }
+
+    //MARK: - Instance
+    private var defaults: NSUserDefaults
+    internal private(set) var versions: [String]
+
+    override init() {
+        self.defaults = NSUserDefaults.standardUserDefaults()
+        self.versions = (self.defaults.arrayForKey(LaunchCountManager.VERSIONS) as? [String]) ?? Array()
+
+        super.init()
+    }
+
+    internal func launch() {
+        if let currentVersion = self.currentVersion() {
+
+            // first, count up
+            var count: Int = self.defaults.integerForKey(currentVersion)
+            self.defaults.setInteger(++count, forKey: currentVersion)
+            // second, check is first time
+            if self.isFirstLaunchForCurrentVersion() {
+                self.versions.append(currentVersion)
+                self.defaults.setObject(self.versions, forKey: LaunchCountManager.VERSIONS)
+            }
+
+            // last, you should synchronize
+            self.defaults.synchronize()
+        }
+    }
+
+    internal func isFirstLaunchForAllVersions() -> Bool {
+        var totalLaunchCount: Int = 0
+        for version in self.versions {
+            totalLaunchCount += self.launchCountForVersion(version)
+            if totalLaunchCount > 1 {
+                return false
+            }
+        }
+        return true
+    }
+
+    internal func isFirstLaunchForCurrentVersion() -> Bool {
+        if let currentVersion = self.currentVersion() {
+            return self.isFirstLaunchForVersion(currentVersion)
+        }
+        return true
+    }
+
+    internal func isFirstLaunchForVersion(version: String) -> Bool {
+        return self.launchCountForVersion(version) == 1 ? true : false
+    }
+
+
+    internal func launchCountForCurrentVersion() -> Int {
+        if let currentVersion = self.currentVersion() {
+            return self.launchCountForVersion(currentVersion)
+        }
+        return 0
+    }
+
+    internal func launchCountForVersion(version: String) -> Int {
+        return self.defaults.integerForKey(version) ?? 0
+    }
+
+    internal func currentVersion() -> String? {
+        if let infoDictionary = NSBundle.mainBundle().infoDictionary {
+            if let shortVersionString: String = infoDictionary["CFBundleShortVersionString"] as? String {
+                return shortVersionString
+            }
+        }
+        return nil
+    }
+}
---
+++ README.md
@@ -0,0 +1,29 @@
+# LaunchCountManager
+
+[![CI Status](http://img.shields.io/travis/ryutamaki/LaunchCountManager-Swift.svg?style=flat)](https://travis-ci.org/ryutamaki/LaunchCountManager-Swift)
+[![Version](https://img.shields.io/cocoapods/v/LaunchCountManager-Swift.svg?style=flat)](http://cocoapods.org/pods/LaunchCountManager-Swift)
+[![License](https://img.shields.io/cocoapods/l/LaunchCountManager-Swift.svg?style=flat)](http://cocoapods.org/pods/LaunchCountManager-Swift)
+[![Platform](https://img.shields.io/cocoapods/p/LaunchCountManager-Swift.svg?style=flat)](http://cocoapods.org/pods/LaunchCountManager-Swift)
+
+## Usage
+
+To run the example project, clone the repo, and run `pod install` from the Example directory first.
+
+## Requirements
+
+## Installation
+
+LaunchCountManager is available through [CocoaPods](http://cocoapods.org). To install
+it, simply add the following line to your Podfile:
+
+```ruby
+pod "LaunchCountManager"
+```
+
+## Author
+
+ryutamaki, ryu.mochi@gmail.com
+
+## License
+
+LaunchCountManager is available under the MIT license. See the LICENSE file for more info.
